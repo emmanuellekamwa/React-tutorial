a3e7a0a6b38ef3d34d13050eb145eaa9b811b20a
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-09-28 17:45:56 +0100

    Implement CSS to react app

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,25 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+  const { completed, id, title } = props.todo
     return (
-        <li>
-          <input type="checkbox" 
-          checked={props.todo.completed}  
-          onChange={() => console.log("clicked")}/> 
-          <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-                Delete
-           </button>
-          {props.todo.title}
-        </li>
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </li>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+//component file
 import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+
+//stylesheet
+import "./App.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
